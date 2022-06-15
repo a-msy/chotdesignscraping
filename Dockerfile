fcc632a4fbad612f694c56857cedae81cb34b228
a-msy <m5s2y5a@gmail.com>
2022-06-16 02:05:45 +0900

    命に嫌われている

--- Dockerfile
+++ Dockerfile
@@ -1,26 +1,12 @@
-FROM python:3
-USER root
+FROM python:3.7-slim-buster
+
+ENV PYTHONIOENCODING utf-8
+ENV TZ="Asia/Tokyo"
+ENV LANG=C.UTF-8
+ENV LANGUAGE=en_US:en
 
 WORKDIR /app
 
-RUN apt-get update
-RUN apt-get -y install locales && localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
-ENV LANG ja_JP.UTF-8
-ENV LANGUAGE ja_JP:ja
-ENV LC_ALL ja_JP.UTF-8
-ENV TZ JST-9
-ENV TERM xterm
+COPY ./requirements.txt /requirements.txt
 
-RUN apt-get install -y vim less
-RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add && \
-wget http://dl.google.com/linux/deb/pool/main/g/google-chrome-unstable/google-chrome-unstable_93.0.4577.18-1_amd64.deb && \
-apt-get install -y -f ./google-chrome-unstable_93.0.4577.18-1_amd64.deb
-
-ADD https://chromedriver.storage.googleapis.com/93.0.4577.15/chromedriver_linux64.zip /opt/chrome/
-RUN cd /opt/chrome/ && unzip chromedriver_linux64.zip
-
-RUN pip install --upgrade pip
-RUN pip install --upgrade setuptools
-RUN pip install selenium && pip install bs4 && pip install oauth2client && chromedriver_binary
-
-ENV PATH /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/chrome
+RUN pip install -r /requirements.txt
--- app/getpdfs.py
+++ app/getpdfs.py
@@ -1,18 +1,10 @@
-import math
-import sys
-import re
-import glob
 import os
 import json
 import time
-import chromedriver_binary
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
-from urllib import request
 
 def PrintSetUp():
     #印刷としてPDF保存する設定
@@ -43,19 +35,17 @@ def PrintSetUp():
     
     prefs = {'printing.print_preview_sticky_settings.appState':
              json.dumps(appState),
-             "download.default_directory": "./downloads"
+             "download.default_directory": '~/Downloads'
              } #appState --> pref
     chopt.add_experimental_option('prefs', prefs) #prefs --> chopt
-    chopt.add_argument("--headless")
     chopt.add_argument('--kiosk-printing') #印刷ダイアログが開くと、印刷ボタンを無条件に押す。
     return chopt
 
 def main_WebToPDF(BlogURL):
     #Web ページもしくはhtmlファイルをPDFにSeleniumを使って変換する
     chopt = PrintSetUp()
-    driver_path = "/opt/chrome/chromedriver" #webdriverのパス
-    driver = webdriver.Chrome(executable_path=driver_path, options=chopt)
-    driver.implicitly_wait(10) # 秒 暗示的待機 
+    driver = webdriver.Remote(command_executor="http://selenium_1:4444/wd/hub", options=chopt)
+    driver.implicitly_wait(5) # 秒 暗示的待機 
     driver.get(BlogURL) #ブログのURL 読み込み
     WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)  # ページ上のすべての要素が読み込まれるまで待機（15秒でタイムアウト判定）
     driver.execute_script('return window.print()') #Print as PDF
